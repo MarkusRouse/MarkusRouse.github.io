652b7f110848baf32595f95cc749089a097c2b1c
Marcos Rosas <marcos.rosas@fvet.edu.uy>
2023-09-21 13:26:15 -0300

    added a new envent for the addImageButton, but is not working yet

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const imageFile = document.getElementById("imageFile");
+const addImageButton = document.getElementById("addImageButton");
+let imageId = 0;
+
+addImageButton.addEventListener("click", () =>{
+    console.log(imageFile.values)
+})
+
+function addImage(event){
+    localStorage.setItem (`Image${imageId}`,imageFile.files[0])
+    imageId++;
+}
+
 function loadImages(){
 
 }
