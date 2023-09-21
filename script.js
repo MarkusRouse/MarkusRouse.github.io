6a0627ff2b92bfc9d5561afb20fe7d0bcf342204
Marcos Rosas <marcos.rosas@fvet.edu.uy>
2023-09-21 13:45:35 -0300

    now when click in addImageButton add the image to the local storage

--- script.js
+++ script.js
@@ -1,31 +1,6 @@
 const imageFile = document.getElementById("imageFile");
 const addImageButton = document.getElementById("addImageButton");
-let imageId = 0;
 
 addImageButton.addEventListener("click", () =>{
-    console.log(imageFile.values)
+    localStorage.setItem (`Image${imageFile.name}`,imageFile.value)
 })
-
-function addImage(event){
-    localStorage.setItem (`Image${imageId}`,imageFile.files[0])
-    imageId++;
-}
-
-function loadImages(){
-
-}
-
-let images = allStorage();
-console.log(images);
-
-
-function allStorage() {
-    let values = [],
-        keys = Object.keys(localStorage),
-        i = keys.length;
-
-    while ( i-- ) {
-        values.push( localStorage.getItem(keys[i]) );
-    }
-    return values;
-}
